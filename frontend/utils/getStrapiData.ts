bb23c995cfa7709a1b62b1b87025bd2c024f2b81
KhromenkoDaniel <daniilkhro.work@gmail.com>
2024-11-29 16:36:57 +0200

    refactor: improve code

--- frontend/utils/getStrapiData.ts
+++ frontend/utils/getStrapiData.ts
@@ -1,30 +1,31 @@
 export async function getStrapiData<T>(
-    endpoint: string,
-    query: Record<string, any> = {}
+  endpoint: string,
+  query: Record<string, unknown> = {},
 ): Promise<T> {
-    const queryString = Object.entries(query)
-        .map(([key, value]) => {
-            if (typeof value === "object") {
-                return Object.entries(value)
-                    .map(([nestedKey, nestedValue]) => `${key}[${nestedKey}]=${nestedValue}`)
-                    .join("&");
-            }
-            return `${key}=${value}`;
-        })
-        .join("&");
+  const queryString = Object.entries(query)
+    .map(([key, value]) => {
+      if (typeof value === "object") {
+        return Object.entries(value as Record<string, unknown>)
+          .map(
+            ([nestedKey, nestedValue]) => `${key}[${nestedKey}]=${nestedValue}`,
+          )
+          .join("&");
+      }
+      return `${key}=${value}`;
+    })
+    .join("&");
 
-    const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/${endpoint}?${queryString}`;
+  const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/${endpoint}?${queryString}`;
 
-    try {
-        const res = await fetch(url);
-        if (!res.ok) {
-            const errorData = await res.json();
-            console.error("Strapi Error:", errorData);
-            throw new Error(`Failed to fetch data: ${res.statusText}`);
-        }
-        return res.json();
-    } catch (error) {
-        console.error("Fetch Error:", error);
-        throw error;
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.error("Strapi Error:", errorData);
     }
+    return res.json();
+  } catch (error) {
+    console.error("Fetch Error:", error);
+    throw error;
+  }
 }
